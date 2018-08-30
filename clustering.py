063d449b43489197b76108c49dc184d0b49658a6
Adam Budai <budai8adam@gmail.com>
2018-08-30 12:01:08 +0200

    Clustering was added.

---
+++ clustering.py
@@ -0,0 +1,21 @@
+import numpy as np
+from sklearn.cluster import MiniBatchKMeans
+
+
+class ClusteringKMeans:
+
+    def __init__(self, num_clusters, batch_size, X=None):
+        self.num_clusters = num_clusters
+        self.batch_size = batch_size
+        self.kmeans = MiniBatchKMeans(num_clusters, batch_size==batch_size)
+        if X is not None:
+            self.kmeans.fit(X)
+
+    def test(self):
+        X = np.random.rand(self.batch_size * 5, 8)
+        self.kmeans.fit(X)
+        print('Test is done!')
+    
+    def predict(self, x):
+        return self.kmeans.predict(x)
+
